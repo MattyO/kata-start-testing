76c1acf8f89181e256d076e9dbbe5d80122eb916
Matt <odonnell004@gmail.com>
2021-05-16 00:54:00 -0400

    test the easy stuff

--- spec/javascripts/app_spec.js
+++ spec/javascripts/app_spec.js
@@ -4,4 +4,15 @@ describe("A suite", function() {
   });
 });
 
+describe("calculateDistance", function(){
+    it("returns the right distance", function(){
+        expect(Math.round(calculateDistance(1, 1, 1, 2))).toEqual(111);
+    })
+})
 
+describe("Number", function(){
+    it(".toRad", function(){
+        var number = 1 
+        expect(number.toRad()).toEqual(Math.PI / 180);
+    })
+})
